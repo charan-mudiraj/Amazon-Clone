ae0c216e193622ddec34ce7c06aa3328e223556a
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2023-10-28 18:36:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
+function App() {
+  return <h1>Sample Text</h1>;
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React, { StrictMode } from "react";
+import ReactDom from "react-dom/client";
+import App from "./App";
+
+ReactDom.createRoot(document.getElementById("root")).render(<App />);
