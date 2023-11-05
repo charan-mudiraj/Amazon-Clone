f18bc12d65427622034fb3ebd274eacf50e61949
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2023-11-05 05:35:02 +0530

    added scrolling feature

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,26 +1,50 @@
 import "./css/ProductsList.css";
 import ProductCard from "../components/product_cards/ProductCard";
+import { useState } from "react";
+
+function ScrollBtn(props) {
+  const leftPath = "products_images/left.png";
+  const rightPath = "products_images/right.png";
+  return (
+    <button class="scrollBtn" id={props.direction} onClick={props.onClick}>
+      <img src={props.direction == "left" ? leftPath : rightPath} />
+    </button>
+  );
+}
 
 function ProductsList() {
-  function ScrollBtn(props) {
-    const leftPath = "products_images/left.png";
-    const rightPath = "products_images/right.png";
-    return (
-      <button class="scrollBtn" id={props.direction}>
-        <img src={props.direction == "left" ? leftPath : rightPath} />
-      </button>
-    );
+  const [scrollPosition, setScrollPosition] = useState(0);
+  function scrollLeft() {
+    if (scrollPosition > 0) {
+      setScrollPosition(scrollPosition - 1);
+    }
   }
+  function scrollRight() {
+    setScrollPosition(scrollPosition + 1);
+  }
+  const plScrollStyle = {
+    transform: "translate(" + -(scrollPosition * (218 + 24) * 5) + "px)",
+    transition: "transform 0.7s",
+  };
   return (
-    <div id="productsListDiv">
-      <ScrollBtn direction="left" />
-      <div id="productsList">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+    <div id="productsListExpanded">
+      <div id="productsListDiv">
+        <div id="productsList" style={plScrollStyle}>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
       </div>
-      <ScrollBtn direction="right" />
+      <ScrollBtn direction="left" onClick={scrollLeft} />
+      <ScrollBtn direction="right" onClick={scrollRight} />
     </div>
   );
 }
