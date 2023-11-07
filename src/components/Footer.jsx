4462aa5560f6f1da4ece6123aeba56e960784c98
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2023-11-07 23:16:10 +0530

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,134 @@
 import "./css/Footer.css";
 
-const descItems = [
+function TopPositionScroller() {
+  return (
+    <button id="f-top-scroller-btn">
+      <p>Back to Top</p>
+    </button>
+  );
+}
+//====================================== Accessibility ==================================
+const accessibilities = [
+  {
+    title: "Get to Know Us",
+    items: ["About Us", "Careers", "Press Releases", "Amazon Science"],
+  },
+  {
+    title: "Connect with Us",
+    items: ["Facebook", "Twitter", "Instagram"],
+  },
+  {
+    title: "Make Money with Us",
+    items: [
+      "Sell on Amazon",
+      "Sell under Amazon Accelerator",
+      "Project and Build Your Brand",
+      "Amazon Global Selling",
+      "Become an Affiliate",
+      "Fu;filment by Amazon",
+      "Advertise your Products",
+      "Amazon Pay on Merchants",
+    ],
+  },
+  {
+    title: "Lets Us Help You",
+    items: [
+      "COVID-19 and Amazon",
+      "Your Account",
+      "Returns Centre",
+      "100% Purchase Protection",
+      "Amazon App Download",
+      "Help",
+    ],
+  },
+];
+function Accessibility() {
+  function AccessList(access) {
+    return (
+      <div id="f-access-list">
+        <p class="f-access-list-title">{access.title}</p>
+        {access.items.map((item) => (
+          <p class="f-access-list-item">{item}</p>
+        ))}
+      </div>
+    );
+  }
+  return <div id="f-access">{accessibilities.map(AccessList)}</div>;
+}
+//====================================== Accessibility ==================================
+
+//====================================== DNS Country and Language ==================================
+const dnsCountries = [
+  "Australia",
+  "Brazil",
+  "Canada",
+  "China",
+  "France",
+  "Germany",
+  "Italy",
+  "Japan",
+  "Mexico",
+  "Netherlands",
+  "Poland",
+  "Singapore",
+  "Spain",
+  "Turkey",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States",
+];
+const languages = [
+  { name: "English", code: "EN" },
+  { name: "हिन्दी", code: "HI" },
+  { name: "தமிழ்", code: "TA" },
+  { name: "తెలుగు", code: "TE" },
+  { name: "ಕನ್ನಡ", code: "KN" },
+  { name: "മലയാളം", code: "ML" },
+  { name: "বাংলা", code: "BM" },
+  { name: "मराठी", code: "MR" },
+];
+function Language() {
+  function LangOption(lang) {
+    return (
+      <option class="f-lang-option">
+        <div>
+          <input type="radio" />
+          <p>
+            {lang.name} - {lang.code}
+          </p>
+        </div>
+      </option>
+    );
+  }
+  const logoPath = "icons_&_logos/logo.png";
+  return (
+    <div id="f-language">
+      <img src={logoPath} alt="logo" />
+      <select id="f-lang-selector">{languages.map(LangOption)}</select>
+    </div>
+  );
+}
+function DNSCountry() {
+  return (
+    <div id="f-dns-links">
+      {dnsCountries.map((country) => (
+        <p>{country}</p>
+      ))}
+    </div>
+  );
+}
+function DNSLang() {
+  return (
+    <div id="f-dns-lang">
+      <Language />
+      <DNSCountry />
+    </div>
+  );
+}
+//====================================== DNS Country and Language ==================================
+
+//====================================== More on Amazon ==================================
+const moaItems = [
   { title: "AdeBooks", desc: "Books, art & collections" },
   { title: "Amazon Web Services", desc: "Scalable Cloud Computing Services" },
   { title: "Audible", desc: "Download Audio Books" },
@@ -13,18 +141,22 @@ const descItems = [
     desc: "100 million songs, ad-free Over 15 million podcast episodes",
   },
 ];
-function DescItem(item) {
+function MoaItem(item) {
   return (
-    <div class="f-desc-item">
-      <a href="" class="f-desc-item-title">
+    <div class="f-moa-item">
+      <a href="" class="f-moa-item-title">
         {item.title}
       </a>
       <br />
-      <a href="" class="f-desc-item-desc">
+      <a href="" class="f-moa-item-desc">
         {item.desc}
       </a>
     </div>
   );
+}
+function MoreOnAmazon() {
+  //moa
+  return <div id="moa-list-grid">{moaItems.map(MoaItem)}</div>;
 }
 function Copyright() {
   return (
@@ -38,14 +170,24 @@ function Copyright() {
     </div>
   );
 }
-function DescItemsList() {
-  return <div id="desc-list-grid">{descItems.map(DescItem)}</div>;
+function MoaCopyright() {
+  return (
+    <div id="f-moa-copyright">
+      <MoreOnAmazon />
+      <Copyright />
+    </div>
+  );
 }
+//====================================== More on Amazon and Copyright ==================================
+
 function Footer(descItems) {
   return (
     <div id="footer">
-      <DescItemsList />
-      <Copyright />
+      <TopPositionScroller />
+      <Accessibility />
+      <hr id="f-hr" />
+      <DNSLang />
+      <MoaCopyright />
     </div>
   );
 }
