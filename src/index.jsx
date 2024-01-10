735000a547b3321908246d180ae4abda30c47087
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2024-01-10 22:10:08 +0530

    -

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,8 @@ import React, { StrictMode } from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
 import "./App.css";
+import { inject } from "@vercel/analytics";
+
+inject();
+
 ReactDom.createRoot(document.getElementById("root")).render(<App />);
